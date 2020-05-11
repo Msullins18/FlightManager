59d16b163bc29d955690330f7463c1703659f0e5
hjeong2020 <64095720+hjeong2020@users.noreply.github.com>
2020-05-11 12:41:40 -0500

    changed

--- src/main/java/com/infy/demo/model/SearchFlights.java
+++ src/main/java/com/infy/demo/model/SearchFlights.java
@@ -1,12 +1,18 @@
 package com.infy.demo.model;
 
 import java.time.LocalDate;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 
 import lombok.Data;
 @Data
 public class SearchFlights {
     private Integer airportId;
     private String destination;
+    @JsonDeserialize(using = LocalDateDeserializer.class)  
+    @JsonSerialize(using = LocalDateSerializer.class)  
     private LocalDate date;
     private Integer numberOfTickets;
 }
