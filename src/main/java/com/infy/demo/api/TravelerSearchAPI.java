5b28e3168249eb1bd1551cb3f61e2fd2884ad508
hjeong2020 <hjeong2020>
2020-05-11 11:17:58 -0500

    changed

--- src/main/java/com/infy/demo/api/TravelerSearchAPI.java
+++ src/main/java/com/infy/demo/api/TravelerSearchAPI.java
@@ -1,27 +1,28 @@
 package com.infy.demo.api;
 
 import java.util.List;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
+import com.infy.demo.exceptions.FlightNotFoundException;
+import com.infy.demo.exceptions.InvalidDateException;
+import com.infy.demo.exceptions.NoFlightsAvailableException;
 import com.infy.demo.model.Airport;
 import com.infy.demo.model.Flight;
 import com.infy.demo.model.SearchFlights;
 import com.infy.demo.service.TravelerSearchService;
 
 import lombok.extern.slf4j.Slf4j;
+
+
 @CrossOrigin
 @RestController
 @RequestMapping(value ="Search")
@@ -45,28 +46,31 @@ public class TravelerSearchAPI {
 	@GetMapping(value="/getAirports")
 	public ResponseEntity<List<Airport>> getAllOrigins() throws Exception{
 		List<Airport> origins = null;
-		try{
-			origins = travelerSearchService.getAllOrigins();
-			ResponseEntity<List<Airport>> response = new ResponseEntity<List<Airport>>(origins, HttpStatus.OK);
-			log.info("The number of airports is " + origins.size());
-			return response;
-		}catch(Exception e){
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND,	environment.getProperty(e.getMessage()), e);
-		}
+		origins = travelerSearchService.getAllOrigins();
+		ResponseEntity<List<Airport>> response = new ResponseEntity<List<Airport>>(origins, HttpStatus.OK);
+		log.info("The number of airports is " + origins.size());
+		return response;
 	}
 
 	@GetMapping(value="/getDestinations")
 	public ResponseEntity<List<String>> getAllDestinations() throws Exception{
 		List<String> destinations = null;
-		try{
-			destinations = travelerSearchService.getAllDestinations();
-			ResponseEntity<List<String>> response = new ResponseEntity<List<String>>(destinations, HttpStatus.OK);
-			log.info("The number of airports is " + destinations.size());
-			return response;
-		}catch(Exception e){
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND,	environment.getProperty(e.getMessage()), e);
-		}
+		destinations = travelerSearchService.getAllDestinations();
+		ResponseEntity<List<String>> response = new ResponseEntity<List<String>>(destinations, HttpStatus.OK);
+		log.info("The number of airports is " + destinations.size());
+		return response;
 	}
+	
+	 
+	 @ExceptionHandler(NoFlightsAvailableException.class)
+	    public ResponseEntity<Object> handleException(NoFlightsAvailableException  e) {
+	    	return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+	    }
+	 
+	 @ExceptionHandler(InvalidDateException.class)
+	    public ResponseEntity<Object> handleException(InvalidDateException  e) {
+	    	return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+	    }
 	
 
 }
